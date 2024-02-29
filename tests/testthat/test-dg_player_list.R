cb3b9c6f05acba1489e4c5917f23065b485a3fe0
Tay Harris <taylor@campwestagency.com>
2024-02-29 15:25:48 -0700

    Fixed testing to use the mockhttp calls.

--- tests/testthat/test-dg_player_list.R
+++ tests/testthat/test-dg_player_list.R
@@ -1,18 +1,24 @@
 test_that("dg_player_list returns a result", {
-  result <- dg_player_list()
-  expect_false(is.null(result), "dg_player_list should not return NULL.")
+  with_mock_api({
+    result <- dg_player_list()
+    expect_false(is.null(result), "dg_player_list should not return NULL.")
+  })
 })
 
 test_that("dg_player_list result is a dataframe", {
-  result <- dg_player_list()
-  expect_is(result, "data.frame", "dg_player_list should return a dataframe.")
+  with_mock_api({
+    result <- dg_player_list()
+    expect_s3_class(result, "data.frame", "dg_player_list should return a dataframe.")
+  })
 })
 
 test_that("dg_player_list result has expected columns", {
-  result <- dg_player_list()
-  expected_cols <- c("amateur", "country", "country_code", "dg_id", "player_name")
+  with_mock_api({
+    result <- dg_player_list()
+    expected_cols <- c("amateur", "country", "country_code", "dg_id", "player_name")
 
-  for (col in expected_cols) {
-    expect_true(col %in% names(result), info = paste("Missing column:", col))
-  }
+    for (col in expected_cols) {
+      expect_true(col %in% names(result), info = paste("Missing column:", col))
+    }
+  })
 })
--- tests/testthat/test-dg_schedule.R
+++ tests/testthat/test-dg_schedule.R
@@ -1,19 +1,25 @@
 test_that("dg_schedule returns a result for 'pga'", {
-  result <- dg_schedule("pga")
-  expect_false(is.null(result))
+  with_mock_api({
+    result <- dg_schedule("pga")
+    expect_false(is.null(result))
+  })
 })
 
 test_that("dg_schedule result is a dataframe", {
-  result <- dg_schedule("pga")
-  expect_s3_class(result, "data.frame")
+  with_mock_api({
+    result <- dg_schedule("pga")
+    expect_s3_class(result, "data.frame")
+  })
 })
 
 test_that("dg_schedule result has expected columns", {
-  result <- dg_schedule("pga")
-  expected_cols <- c("course", "course_key", "event_id", "event_name",
-                     "latitude", "location", "longitude", "start_date")
+  with_mock_api({
+    result <- dg_schedule("pga")
+    expected_cols <- c("course", "course_key", "event_id", "event_name",
+                       "latitude", "location", "longitude", "start_date")
 
-  for (col in expected_cols) {
-    expect_true(col %in% names(result), info = paste("Missing column:", col))
-  }
+    for (col in expected_cols) {
+      expect_true(col %in% names(result), info = paste("Missing column:", col))
+    }
+  })
 })
