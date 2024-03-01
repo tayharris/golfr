a69c6264604e6dc6a99c95196cc22f928f059df0
Tay Harris <taylor@campwestagency.com>
2024-03-01 15:01:33 -0700

    added prob to odds to namespace and a test

--- R/prob_to_odds.R
+++ R/prob_to_odds.R
@@ -1,3 +1,9 @@
+#' Converts a Probability to American Odds.
+#'
+#' @param probability a probability value.
+#' @return the american odds for that probability.
+#' @export
+
 prob_to_odds <- function(probability) {
   ifelse(probability >= 0.5,
          -((probability / (1 - probability)) * 100),  # Favorite
